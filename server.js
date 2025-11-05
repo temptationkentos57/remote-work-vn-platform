bf62a81593d1c599fa459940428ccbfa78e0a2b7
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-05 12:46:42 +0100

    Update MongoDB connection URI logging for clarity.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = socketIo(server);
 // Kết nối đến MongoDB
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/remoteWorkVN';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Kết nối đến MongoDB thành công'))
+  .then(() => console.log('Kết nối thành công đến MongoDB: ' + mongoURI))
   .catch(err => console.error('Lỗi kết nối tới MongoDB:', err));
 
 app.get('/', (req, res) => {
