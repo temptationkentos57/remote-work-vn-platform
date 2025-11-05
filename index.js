5079369cf43e9e98f3f373571830c67b0b92da06
temptationkentos57 <kreetlannoo@hotmail.com>
2025-11-05 12:47:44 +0100

    Update MongoDB connection URI to use environment variable

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
-mongoose.connect('mongodb://localhost:27017/remoteWorkVN', { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/remoteWorkVN';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Kết nối đến MongoDB thành công'))
   .catch(err => console.error('Lỗi kết nối tới MongoDB:', err));
 
